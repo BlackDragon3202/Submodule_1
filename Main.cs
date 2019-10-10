b04dbbc53a0a73190d3e3e46f4ee2a7094949c8c
Mark <menshih_mark@mail.ru>
2019-10-10 10:15:22 +0300

    Add Main.cs

---
+++ Main.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+namespace Submodule_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
